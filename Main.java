2778c654c995fe98df4c3b2e4544ca589eade878
Mayank malviya <mayankmalviya440@gmail.com>
2026-02-15 10:35:16 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,72 @@
+import java.util.LinkedList;
 
+class Buffer {
+    private LinkedList<Integer> list = new LinkedList<>();
+    private int capacity = 5;
+
+    public synchronized void produce(int value) throws InterruptedException {
+        while (list.size() == capacity)      {
+            wait();
+        }
+        list.add(value);
+        System.out.println("Produced: " + value);
+        notify();
+    }
+
+    public synchronized void consume() throws InterruptedException {
+        while (list.isEmpty()) {
+            wait();
+        }
+        int value = list.removeFirst();
+        System.out.println("Consumed: " + value);
+        notify();
+    }
+}
+
+class Producer extends Thread {
+    Buffer buffer;
+
+    Producer(Buffer buffer) {
+        this.buffer = buffer;
+    }
+
+    public void run() {
+        int i = 1;
+        try {
+            while (true) {
+                buffer.produce(i++);
+                Thread.sleep(500);
+            }
+        } catch (Exception e) {
+        }
+    }
+}
+
+class Consumer extends Thread {
+    Buffer buffer;
+
+    Consumer(Buffer buffer) {
+        this.buffer = buffer;
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                buffer.consume();
+                Thread.sleep(800);
+            }
+        } catch (Exception e) {
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Producer Consumer Problem  developed by Monty");
+        Buffer buffer = new Buffer();
+        Producer p = new Producer(buffer);
+        Consumer c = new Consumer(buffer);
+        p.start();
+        c.start();
+    }
+}
